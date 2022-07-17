1b3a6bb9049ddc78bbb65f05ed3fbafcc5c2b8ae
Nicholas Ang <ang.tze.yong@gmail.com>
2022-07-17 20:26:45 +0800

    Create react app

---
+++ auth_template/frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const yeet = {
+  x : 'yass',
+}
+
+const element = 
+<><h3>yeet.x {yeet.x}</h3>
+<p>poggers</p></>
+
+function App() {
+  return (
+    element
+  );
+}
+
+export default App;
---
+++ auth_template/frontend/src/pages/login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyComponent = (props) => {
+  return (
+    <>{props.name}</>
+  )
+}
+
+export default MyComponent;

bb8244792c6a3c5c5f9e85e6b14efd0313888e81
Nicholas Ang <ang.tze.yong@gmail.com>
2022-07-28 11:54:58 +0800

    Final commit

--- auth_template/backend/authapi/permissions.py
+++ auth_template/backend/authapi/permissions.py
@@ -6,16 +6,21 @@ Permissions as implemented by the AuthorityMatrix.
 '''
 class IsEndUser(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user.role in ['EU']
 
 
 class IsInstitution(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user.role in ['IN']
 
 
 class IsInstitutionOrEndUser(BasePermission):
-    def has_permission(self, request, view):
-        #print(request.user)
+    def has_permission(self, request, view):        
+        if request.user.is_anonymous:
+            return False
         return request.user.role in ['IN', 'EU']
 

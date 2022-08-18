f7f13ef17b8b9e29f66d20601dc57cdee80d12d7
Nicholas Ang <ang.tze.yong@gmail.com>
2022-08-18 11:57:39 +0800

    Remove secret key from settings

--- auth_template/backend/authtemplate/settings.py
+++ auth_template/backend/authtemplate/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k$p-7rkl)qb81@q^@ssczn35f1c%^xv&5j6a2o6axm94t*bmta'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
